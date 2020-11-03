85b6e2adb1ec3fa469a23bc5521eea1429dd0441
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-11-03 12:01:14 +0800

    install react-router-dom and add button to route to task creation page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { TaskContainer } from '../../containers/TaskContainer/TaskContainer';
 
 export const Layout = () => {
   return (
     <div>
-      <TaskContainer />
+      <Route exact path="/">
+        <TaskContainer />
+      </Route>
+      <Route path="/task">
+        <div>new Task</div>
+      </Route>
+
+
     </div>
   );
 }
